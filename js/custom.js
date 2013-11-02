4b5c967485fc5174ab9db9a8bef2c44f55ae0909
otech47 <otech47@gmail.com>
2013-11-01 18:34:27 -0700

    lots of changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,80 @@
 $(document).ready( function() {
+	function getEventTags()
+	{
+		var artistSelection = $("input[id='artists']").val();
+		var postdata = { artist:artistSelection };
+		jQuery.ajax({
+			type: "POST",
+			url: '../scripts/eventTags.php',
+			data: postdata,
+			success: function(data) 
+			{
+				var autocompleteData = JSON.parse(data);
+				$("input[id='events']").autocomplete({
+					source: autocompleteData
+				});
+				$("#events").select();
+				$("#events").autocomplete("search", "");
+			}
+		});
+	}
+	function getArtistTags()
+	{
+		var eventSelection = $("input[id='events']").val();
+		var postdata = { event:eventSelection };
+		jQuery.ajax({
+			type: "POST",
+			url: '../scripts/artistTags.php',
+			data: postdata,
+			success: function(data) 
+			{
+				var autocompleteData = JSON.parse(data);
+				$("input[id='artists']").autocomplete({
+					source: autocompleteData
+				});
+				$("#artists").select();
+				$("#artists").autocomplete("search", "");
+			}
+		});
+	}
+	function getAllTags()
+	{
+		var autocompleteData = new Array();
+		var eventSelection = "";
+		var artistSelection = "";
+		var postdata = { event:eventSelection };
+		jQuery.ajax({
+			type: "POST",
+			url: '../scripts/artistTags.php',
+			data: postdata,
+			success: function(data) 
+			{
+				var autocompleteDataTemp = JSON.parse(data);
+				autocompleteData = autocompleteData.concat(autocompleteDataTemp);
+			}
+		});
+		postdata = { artist:artistSelection };
+		jQuery.ajax({
+			type: "POST",
+			url: '../scripts/eventTags.php',
+			data: postdata,
+			success: function(data) 
+			{
+				var autocompleteDataTemp = JSON.parse(data);
+				autocompleteData = autocompleteData.concat(autocompleteDataTemp);
+			}
+		});
+		$("input[id='select-combined']").autocomplete({
+					source: autocompleteData
+				});
+		$("#select-combined").select();
+		$("#select-combined").autocomplete("search", "");
+	}
+	$("#select-combined").autocomplete({
+		minLength: 0
+	}).click(function() {
+		getAllTags();
+	});
 	$(".option-button").hover(function(){
 		$(this).css("box-shadow","0 0 2px 5px grey inset")
 		},
@@ -6,7 +82,11 @@ $(document).ready( function() {
 		{
 		$(this).css("box-shadow","0 0 0 0")
 	});
-	$(".option-button").click(function(){
-		
+	$(".random-button").click(function(){
+		$(".option-button").css("box-shadow","0 0 0 0");
+		$(".option-label").animate({opacity:'0'}, 200, function() {
+			$(".select-buttons").css("display","none");
+			$(".combined-search").css("display","table");
+			});
 	});
 });
